26cf04e191380c0a2bd8ddd253db4eb6410b05ca
JoaoASousa <joaosousa.pt@gmail.com>
2022-04-19 11:53:13 +0100

    Basic Setup/takeDown methods for BaseAgent

--- app/src/main/java/asma_proj1/agents/BaseAgent.java
+++ app/src/main/java/asma_proj1/agents/BaseAgent.java
@@ -5,7 +5,19 @@ import jade.core.Agent;
 public class BaseAgent extends Agent {
     private int capital = 0;
 
+    protected void setup() {
+        System.out.println("Setting up BaseAgent...");
+        System.out.println("BaseAgent " + getAID().getName() + " is ready!");
+    }
+
+    // Put agent clean-up operations here
+    protected void takeDown() {
+        System.out.println("Base agent " + getAID().getName() + " terminating.");
+    }
+
     public int getCapital() {
         return capital;
     }
+
+
 }
