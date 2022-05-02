c8e2483b72e1731fe3bf59af4bbd4d76e0fd37ae
Gonçalo Pascoal <goncalojpascoal@gmail.com>
2022-05-02 17:32:17 +0100

    Implement GenerateCardSet behavior

--- app/src/main/java/asma_proj1/agents/BaseAgent.java
+++ app/src/main/java/asma_proj1/agents/BaseAgent.java
@@ -1,11 +1,29 @@
 package asma_proj1.agents;
 
+import jade.core.AID;
 import jade.core.Agent;
+import jade.core.ServiceException;
+import jade.core.behaviours.CyclicBehaviour;
+import jade.core.messaging.TopicManagementHelper;
+import jade.lang.acl.ACLMessage;
+import jade.lang.acl.MessageTemplate;
 
 public class BaseAgent extends Agent {
     private int capital = 0;
+    private AID topic;
 
     protected void setup() {
+        try {
+            TopicManagementHelper helper = (TopicManagementHelper) getHelper(TopicManagementHelper.SERVICE_NAME);
+            topic = helper.createTopic(CardDatabase.TOPIC_NAME);
+            helper.register(topic);
+        }
+        catch (ServiceException e) {
+            e.printStackTrace();
+        }
+
+        addBehaviour(new HandleDatabaseMessages(this));
+
         System.out.println("Setting up BaseAgent...");
         System.out.println("BaseAgent " + getAID().getName() + " is ready!");
     }
@@ -19,6 +37,10 @@ public class BaseAgent extends Agent {
         return capital;
     }
 
+    public AID getTopic() {
+        return topic;
+    }
+
     public boolean changeCapital(int delta) {
         int newCapital = capital + delta;
         
@@ -27,5 +49,25 @@ public class BaseAgent extends Agent {
         }
 
         return false;
+    }
+
+    private class HandleDatabaseMessages extends CyclicBehaviour {
+        private BaseAgent agent;
+
+        public HandleDatabaseMessages(BaseAgent agent) {
+            this.agent = agent;
+        }
+
+        @Override
+        public void action() {
+            ACLMessage message = agent.receive(MessageTemplate.MatchTopic(agent.getTopic()));
+            if (message != null) {
+                System.out.println(
+                    "Agent " + myAgent.getLocalName() + 
+                    ": Message about topic " + topic.getLocalName() + 
+                    " received. Content is " + message.getContent()
+                );
+            }
+        }
     }
 }
--- app/src/main/java/asma_proj1/agents/CardDatabase.java
+++ app/src/main/java/asma_proj1/agents/CardDatabase.java
@@ -3,14 +3,57 @@ package asma_proj1.agents;
 import java.util.HashMap;
 import java.util.Map;
 
+import jade.core.AID;
 import jade.core.Agent;
+import jade.core.ServiceException;
+import jade.core.behaviours.TickerBehaviour;
+import jade.core.messaging.TopicManagementHelper;
+import jade.lang.acl.ACLMessage;
 
+import asma_proj1.agents.protocol.CardInfoRespond;
 import asma_proj1.card.Card;
 
 public class CardDatabase extends Agent {
-    private Map<Integer, Card> cards = new HashMap<>();
+    public static final String TOPIC_NAME = "database";
+
+    private AID topic;
+    private final Map<Integer, Card> cards = new HashMap<>();
+
+    @Override
+    protected void setup() {
+        try {
+            TopicManagementHelper helper = (TopicManagementHelper) getHelper(TopicManagementHelper.SERVICE_NAME);
+            topic = helper.createTopic(TOPIC_NAME);
+        }
+        catch (ServiceException e) {
+            e.printStackTrace();
+        }
+
+        addBehaviour(new GenerateCardSet(this));
+        addBehaviour(new CardInfoRespond(this));
+    }
 
     public Card getCardById(int id) {
         return cards.get(id);
+    }
+
+    public void addCard(Card card) {
+        cards.put(card.getId(), card);
+    }
+
+    private class GenerateCardSet extends TickerBehaviour {
+        private static final int intervalSeconds = 60;
+
+        public GenerateCardSet(CardDatabase database) {
+            super(database, intervalSeconds * 1000);
+        }
+
+        @Override
+        protected void onTick() {
+            ACLMessage message = new ACLMessage(ACLMessage.INFORM);
+            message.addReceiver(topic);
+            message.setContent("New card set");
+            send(message);
+        }
     }
 }
