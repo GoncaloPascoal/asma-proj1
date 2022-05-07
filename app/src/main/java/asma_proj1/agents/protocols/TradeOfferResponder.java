ba60b3fdd7318760812dc224f05632b4eeb0bccf
Joao Sousa <joaosousa.pt@gmail.com>
2022-05-07 01:00:36 +0100

    Tried starting TradeOfferResponder

---
+++ app/src/main/java/asma_proj1/agents/protocols/TradeOfferResponder.java
@@ -0,0 +1,22 @@
+package asma_proj1.agents.protocols;
+
+import asma_proj1.agents.CardOwner;
+import jade.domain.FIPAAgentManagement.FailureException;
+import jade.domain.introspection.ACLMessage;
+import jade.proto.ContractNetResponder;
+import jade.proto.SSContractNetResponder;
+
+public class TradeOfferResponder extends SSContractNetResponder {
+    private final TradeOfferData data;
+
+    public TradeOfferResponder(CardOwner target, TradeOfferData data) {
+        super(target, null);
+        this.data = data;
+    }
+
+    @Override
+    protected ACLMessage handleAcceptProposal(ACLMessage cfp, ACLMessage propose, ACLMessage accept) throws FailureException {
+        
+    }
+    
+}
